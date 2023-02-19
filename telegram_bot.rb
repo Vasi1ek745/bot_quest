ca2c4924542fb30ef6143a3f0c9fd3f23b58031f
vasiliy <vasi1ek745@gmail.com>
2023-02-19 23:08:07 +0500

    add data file

--- telegram_bot.rb
+++ telegram_bot.rb
@@ -4,15 +4,18 @@ require_relative "status_check"
 require_relative "user_message"
 
 token = '5801402177:AAGF587t7nv2rGnoiz9EvbEn_i7Z8wr2E9Y'
+questions = File.new("./data/questions.txt","r").readlines
+answers = File.new("./data/answers.txt","r").readlines
 
 status = StatusCheck.new
-user_message = UserMessage.new(["test1","test2"])
+user_message = UserMessage.new(questions,answers)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    if message 
+      if message
+      status.next_step if user_message.check_user_message(message.text, status.step_number)
       
-      bot.api.send_message(chat_id: message.chat.id, text: user_message.message_for_user(status.step_number, message.text) )
+      bot.api.send_message(chat_id: message.chat.id, text: user_message.message_for_user(status.step_number) )
     
     end
   end
--- user_message.rb
+++ user_message.rb
@@ -3,12 +3,12 @@ class UserMessage
 		@questions = questions
 		@answers = answers
 	end
-	def message_for_user(status, user_text)
-		if user_text.downcase == "привет"
-			"Привет"
-		else
-			"Пошел нах быдло"
-		end
+
+	def check_user_message(user_message,status)
+		user_message == @answers[status]
+	end
+	def message_for_user(status)
+		@questions[status]
 	end
 
 
