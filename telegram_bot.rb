6e060414481ebaed21e3f62d5942abafc1b9d5cd
Vasi1ek745 <vasi1ek745@gmail.com>
2023-02-19 14:29:19 +0500

    add new schema create status and message rb

--- telegram_bot.rb
+++ telegram_bot.rb
@@ -1,12 +1,14 @@
+# require_relative 'quest_ng'
 require 'telegram/bot'
 
 token = '5801402177:AAGF587t7nv2rGnoiz9EvbEn_i7Z8wr2E9Y'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Добрый день! Это праздничная игра. Цель получить подарок на новый год вы готовы??")
+    if message 
+      status = status_check
+
+      bot.api.send_message(chat_id: message.chat.id, text: text_for_user(status) )
     
     end
   end
