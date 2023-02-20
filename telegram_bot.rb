0cd2cda4834ebce53cb7acfac5124483690df278
vasiliy <vasi1ek745@gmail.com>
2023-02-20 22:20:00 +0500

    add wrong right answer

--- telegram_bot.rb
+++ telegram_bot.rb
@@ -6,17 +6,24 @@ require_relative "user_message"
 token = '5801402177:AAGF587t7nv2rGnoiz9EvbEn_i7Z8wr2E9Y'
 questions = File.new("./data/questions.txt","r").readlines
 answers = File.new("./data/answers.txt","r").readlines
+right_answer = File.new("./data/right_answer.txt","r").readlines
+wrong_answer = File.new("./data/wrong_answer.txt","r").readlines
 
 status = StatusCheck.new
-user_message = UserMessage.new(questions,answers)
+user_message = UserMessage.new(questions,answers,right_answer, wrong_answer)
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-      if message
-      status.next_step if user_message.check_user_message(message.text, status.step_number)
+      
+      if user_message.check_user_message(message.text, status.step_number)
+          status.next_step
+          bot.api.send_message(chat_id: message.chat.id, text: user_message.right_answer)
+      else
+        
+        bot.api.send_message(chat_id: message.chat.id, text: user_message.wrong_answer)
+      end
       
       bot.api.send_message(chat_id: message.chat.id, text: user_message.message_for_user(status.step_number) )
-    
-    end
+
   end
 end
--- user_message.rb
+++ user_message.rb
@@ -1,11 +1,19 @@
 class UserMessage
-	def initialize(questions,answers=[])
+	def initialize(questions,answers=[],right_answer=[], wrong_answer=[])
 		@questions = questions
 		@answers = answers
+		@right_answer = right_answer
+		@wrong_answer = wrong_answer
 	end
 
 	def check_user_message(user_message,status)
 		user_message == @answers[status]
+	end
+	def right_answer
+		@right_answer.sample		
+	end
+	def wrong_answer
+		@wrong_answer.sample
 	end
 	def message_for_user(status)
 		@questions[status]
